a1286fb77f835f777b739ef94c4d0bc31dc783e6
ArtyProf <rd.artyrchik@gmail.com>
2020-07-06 11:33:33 +0300

    Implement Insertion Sort

--- Program.Sorting.cs
+++ Program.Sorting.cs
@@ -6,7 +6,26 @@ namespace Algorithms
     {
         static void Sorting()
         {
-            // Game init here
+            //Insertion sort
+            //This algorithm works effectively when sorting a small number of elements.
+            int[] data = new int[] { 7, 3, 5, 1, 6, 2, 9, 4 };
+            Console.WriteLine("Insertion Sort");
+            Console.WriteLine("Source array: {0}", string.Join(", ", data));
+            for (int i = 1; i < data.Length; ++i)
+            {
+                int current = data[i];
+                int pointer = i - 1;
+
+                while (pointer >= 0 && data[pointer] > current)
+                {
+                    data[pointer + 1] = data[pointer];
+                    pointer = pointer - 1;
+                }
+
+                data[pointer + 1] = current;
+            }
+
+            Console.WriteLine("Sorted array: {0} \n", string.Join(", ", data));
         }
     }
 }
